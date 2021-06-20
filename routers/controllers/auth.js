c91a53a8dc55022b5fc9e10f6b19c1117b465c4c
MohammadShoman <mohammad.shoman25@gmail.com>
2021-06-19 17:42:07 -0700

    login done

--- routers/controllers/auth.js
+++ routers/controllers/auth.js
@@ -1,21 +1,37 @@
-const usersModel = require('./../../db/models/users');
-const db=require("./../../db/db")
+const usersModel = require("./../../db/models/users");
+const db = require("./../../db/db");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+
+
 const login = (req, res) => {
-	const { email, password } = req.body;
+  const { email, password } = req.body;
+  const query = `SELECT * FROM users WHERE email=?;`;
+  const array = [email];
+  db.query(query, array, (err, result) => {
+    if (err) throw err;
 
-	usersModel
-		.authenticateBasic(email, password)
-		.then((result) => {
-			if (result[1] === 200)
-				return res.status(result[1]).json({ token: result[0] });
+    if (result[0]) {
+      bcrypt.compare(password, result[0].password, (err, result_1) => {
+        if (result_1 === true) {
+          const payload = {
+            id: result[0].id,
+            role: result[0].role,
+          };
+          const secret = process.env.SECRET;
+          const option = { expiresIn: "60m" };
 
-			res.status(result[1]).json(result[0]);
-		})
-		.catch((err) => {
-			res.send(err);
-		});
+          res.json(jwt.sign(payload, secret, option));
+        }else{
+			res.send("wrong password")
+		}
+      });
+    }
+  });
 };
 
 module.exports = {
-	login,
+  login,
 };
